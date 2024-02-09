0246eb59ba5868eda018451c6d0202164fe3e718
leera01 <leera01@users.noreply.github.com>
2024-02-09 14:08:15 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,69 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        /*// Literal Value
+        Console.WriteLine("Hello World");
+        Console.WriteLine('A');
+        Console.WriteLine(2024);
+        Console.WriteLine(2.25);
+        Console.WriteLine(true);
+        Console.WriteLine();
+
+        // Declare variables as strongly type
+        string message = "Hello World";
+        Console.WriteLine(message);
+        char symbol = 'C';
+        Console.WriteLine(symbol);
+        int year = 2024;
+        Console.WriteLine(year);
+        double grade = 2.25;
+        Console.WriteLine(grade);
+        bool isComplete = true;
+        Console.WriteLine(isComplete);
+        Console.WriteLine();*/
+
+        // Reassign the value of a variable
+        string firstName;
+        firstName = "Ely";
+        Console.WriteLine(firstName);
+        firstName = "Raymond";
+        Console.WriteLine(firstName);
+        firstName = "Buddy";
+        Console.WriteLine(firstName);
+        firstName = "Marcus";
+        Console.WriteLine(firstName);
+        Console.WriteLine();
+
+        /*// Using character escape sequences
+        Console.WriteLine("Hello\nWorld!");
+        Console.WriteLine("Hello\tWorld!");
+        Console.WriteLine("Hello \"World\"!");
+        Console.WriteLine("c:\\source\\repos");
+        Console.WriteLine(@"    c:\source\repos    
+      (this is where your code goes)");
+
+        // Using string concatenation
+        firstName = "Bob";
+        string greeting = "Hello";
+        Console.WriteLine(greeting + " " + firstName + "!");
+
+        // Using string interpolation
+        Console.WriteLine($"{greeting} {firstName}!");
+
+        // Mathematical Operations
+        int sum = 7 + 5;
+        int difference = 7 - 5;
+        int product = 7 * 5;
+        decimal quotient = 7m / 5m;
+        int remainder = 7 % 5;
+
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Difference: " + difference);
+        Console.WriteLine("Product: " + product);
+        Console.WriteLine("Quotient: " + quotient);
+        Console.WriteLine("Remainder: " + remainder);*/
+    }
+}
